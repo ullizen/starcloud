427601f096fba0ff4a6cee0a384cc0d36ca99d89
Ulrika Andersson <andersson.au@gmail.com>
2018-03-29 21:17:43 +0200

    Added star rating icons

--- src/scripts/starRating.js
+++ src/scripts/starRating.js
@@ -1,7 +1,42 @@
-$(document).ready(function(){
+(function(){
+	var rating = 0;
 
-	$("#star-1").click(function(){
-		console.log("click");
+	$(".star-input").children("i").click(function(){
+		if (rating == $(this).attr("data-value") && $(this).hasClass("star"))
+		{
+			rating = 0;
+			emptyStars();
+		} 
+		else 
+		{
+			rating = $(this).attr("data-value");
+			renderStars(rating);
+		}
+
+		$("#rating").val(rating);
 	})
 
-});
+	var renderStars = function(rating) {
+
+		var stars = $(".star-input").children("i").filter(function() {
+		    return  $(this).attr("data-value") <= rating;
+		});
+
+		var emptystars = $(".star-input").children("i").filter(function() {
+		    return  $(this).attr("data-value") > rating;
+		});
+
+		stars.addClass("star");
+		stars.removeClass("star-empty");
+
+		emptystars.removeClass("star");
+		emptystars.addClass("star-empty");
+	}
+
+	var emptyStars = function(){
+		var stars = $(".star-input").children("i");
+		stars.removeClass("star");
+		stars.addClass("star-empty");
+	}
+
+})();
