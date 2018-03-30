611a76ebc01f4a8fa6f889850660099a5258ada4
winrar-rar <c.jan.andersson@gmail.com>
2018-03-30 11:13:17 +0200

    log

--- chalicelib/validate.py
+++ chalicelib/validate.py
@@ -1,9 +1,11 @@
 import re
 
 def validate_subject(string):
+    print("validate string = ")
+    print (string)
     if re.match(r'^[a-zA-Z åäöÅÄÖ 0-9+:0-9+]{1,30}*$', string):    
         print(True)
         return True
     else:
-    print(False)
-    return False
+        print(False)
+        return False
