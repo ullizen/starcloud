5989edb89c7ccf26b8d91cf9fa8045e242c59d36
winrar-rar <c.jan.andersson@gmail.com>
2018-03-30 11:15:40 +0200

    fixed typo

--- chalicelib/validate.py
+++ chalicelib/validate.py
@@ -3,7 +3,7 @@ import re
 def validate_subject(string):
     print("validate string = ")
     print (string)
-    if re.match(r'^[a-zA-Z åäöÅÄÖ 0-9+:0-9+]{1,30}*$', string):    
+    if re.match(r'^[a-zA-Z0-9. åäöÅÄÖ]{1,30}*$', string):    
         print(True)
         return True
     else:
